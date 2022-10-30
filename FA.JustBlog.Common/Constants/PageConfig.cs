89522a8f25c3870f384118bb40d75b809b0c3c7c
NGUYEN DUC BAO SON <son191202340@st.utc.edu.vn>
2022-10-30 18:24:11 +0700

    commit

---
+++ FA.JustBlog.Common/Constants/PageConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Common.Constants
+{
+    public class PageConfig
+    {
+        public const int CurentPage = 1;
+        public const int PageSize = 10;
+    }
+}
